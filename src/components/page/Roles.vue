bae4ac2f871c1a1d4eb712007cc40a4e838a0910
ethan-liu <ethan-liu@outlook.com>
2017-06-02 18:05:45 +0800

    修改角色管理页面布局

--- src/components/page/Roles.vue
+++ src/components/page/Roles.vue
@@ -7,41 +7,36 @@
             </el-breadcrumb>
         </div>
 
-        <!-- Main content -->
-        <section class="content">
-            <el-row>
-                <el-button type="primary">新建角色</el-button>
-            </el-row>
-            <el-row>
-                <el-table :data="tableData" border style="width: 50%">
-                    <el-table-column prop="id" label="ID" width="120">
-                    </el-table-column>
-                    <el-table-column prop="name" label="角色名称" width="180">
-                    </el-table-column>
-                    <el-table-column prop="createTime" label="创建时间" sortable width="220">
-                    </el-table-column>
-                    <el-table-column label="操作">
-                        <template scope="scope">
-                            <el-button size="small" type="info"
-                                       @click="handleEdit(scope.$index, scope.row)">编辑
-                            </el-button>
-                            <el-button size="small" type="danger"
-                                       @click="handleDelete(scope.$index, scope.row)">删除
-                            </el-button>
-                        </template>
-                    </el-table-column>
-                </el-table>
-                <div class="pagination" style="width: 50%">
-                    <el-pagination
-                            @current-change="handleCurrentChange"
-                            layout="prev, pager, next"
-                            :total="50">
-                    </el-pagination>
-                </div>
-            </el-row>
-
-        </section>
-        <!-- /.content -->
+        <el-row>
+            <el-button type="primary">新建角色</el-button>
+        </el-row>
+        <el-row>
+            <el-table :data="tableData" border style="width: 50%">
+                <el-table-column prop="id" label="ID" width="120">
+                </el-table-column>
+                <el-table-column prop="name" label="角色名称" width="180">
+                </el-table-column>
+                <el-table-column prop="createTime" label="创建时间" sortable width="220">
+                </el-table-column>
+                <el-table-column label="操作">
+                    <template scope="scope">
+                        <el-button size="small" type="info"
+                                   @click="handleEdit(scope.$index, scope.row)">编辑
+                        </el-button>
+                        <el-button size="small" type="danger"
+                                   @click="handleDelete(scope.$index, scope.row)">删除
+                        </el-button>
+                    </template>
+                </el-table-column>
+            </el-table>
+            <div class="pagination" style="width: 50%">
+                <el-pagination
+                        @current-change="handleCurrentChange"
+                        layout="prev, pager, next"
+                        :total="50">
+                </el-pagination>
+            </div>
+        </el-row>
     </div>
 </template>
 
@@ -85,6 +80,7 @@
     .el-row {
         margin-bottom: 20px;
     }
+
     .el-row :last-child {
         margin-bottom: 0;
     }
