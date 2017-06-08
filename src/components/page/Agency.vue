3c2ba076d84f615c78993ced6f5b2e3017ab7d9d
ethan-liu <ethan-liu@outlook.com>
2017-06-08 11:45:03 +0800

    添加中介、门店、经纪人、申请管理等页面

---
+++ src/components/page/Agency.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <div class="crumbs">
+            <el-breadcrumb separator="/">
+                <el-breadcrumb-item><i class="fa fa-dashboard"></i> 控制台</el-breadcrumb-item>
+                <el-breadcrumb-item>中介管理</el-breadcrumb-item>
+            </el-breadcrumb>
+        </div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                tableData: [],
+                cur_page: 1
+            }
+        },
+        created(){
+            this.getData();
+        },
+        methods: {
+            handleCurrentChange(val){
+                this.cur_page = val;
+                this.getData();
+            },
+            getData(){
+                let self = this;
+                this.axios.get('/api/roles', {
+                    params: {
+                        page: self.cur_page
+                    }
+                }).then((res) => {
+                    self.tableData = res.data.roles;
+                })
+            },
+            handleEdit(index, row) {
+                this.$message('编辑第' + (index + 1) + '行');
+            },
+            handleDelete(index, row) {
+                this.$message.error('删除第' + (index + 1) + '行');
+            },
+        }
+    }
+</script>
+
+<style>
+
+</style>
