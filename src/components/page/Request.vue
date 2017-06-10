e1a5f7ff94773da8a1f2e16627366ece28ffc6ef
ethan-liu <ethan-liu@outlook.com>
2017-06-10 16:51:40 +0800

    修改分期申请页面

--- src/components/page/Request.vue
+++ src/components/page/Request.vue
@@ -8,25 +8,21 @@
         </div>
 
         <el-row>
-            <el-button type="primary">新增</el-button>
-            <el-button type="primary">修改</el-button>
-            <el-button type="primary">删除</el-button>
-            <el-button type="primary">停用</el-button>
-            <el-button type="primary">隐藏停用</el-button>
+            <el-button type="primary">补充／修改申请信息</el-button>
         </el-row>
         <el-row>
             <el-form :inline="true" :model="form">
-                <el-form-item label="中介编号：">
+                <el-form-item label="申请编号：">
                     <el-input v-model="form.id" placeholder="支持模糊查询"></el-input>
                 </el-form-item>
-                <el-form-item label="中介名称：">
+                <el-form-item label="申请日期：">
                     <el-input v-model="form.name" placeholder="支持模糊查询"></el-input>
                 </el-form-item>
-                <el-form-item label="状态：">
-                    <el-select v-model="form.status" placeholder="请选择">
-                        <el-option label="启用" value="1"></el-option>
-                        <el-option label="停用" value="0"></el-option>
-                    </el-select>
+                <el-form-item label="租客姓名：">
+                    <el-input v-model="form.name" placeholder="支持模糊查询"></el-input>
+                </el-form-item>
+                <el-form-item label="门店：">
+                    <el-input v-model="form.name" placeholder="支持模糊查询"></el-input>
                 </el-form-item>
                 <el-form-item>
                     <el-button type="primary" @click="Search">查询</el-button>
@@ -45,19 +41,47 @@
                 </el-table-column>
                 <el-table-column
                         prop="code"
-                        label="中介编号">
+                        label="申请编号">
+                </el-table-column>
+                <el-table-column
+                        prop="code"
+                        label="申请日期">
                 </el-table-column>
                 <el-table-column
                         prop="name"
-                        label="中介名称">
+                        label="租客姓名">
                 </el-table-column>
                 <el-table-column
                         prop="branchAmount"
-                        label="门店数量">
+                        label="联系方式">
+                </el-table-column>
+                <el-table-column
+                        prop="branchAmount"
+                        label="月租金">
+                </el-table-column>
+                <el-table-column
+                        prop="branchAmount"
+                        label="租期">
+                </el-table-column>
+                <el-table-column
+                        prop="branchAmount"
+                        label="手续费（元／期）">
+                </el-table-column>
+                <el-table-column
+                        prop="branchAmount"
+                        label="总金额">
+                </el-table-column>
+                <el-table-column
+                        prop="branchAmount"
+                        label="经纪人">
+                </el-table-column>
+                <el-table-column
+                        prop="branchAmount"
+                        label="门店名称">
                 </el-table-column>
                 <el-table-column
                         prop="enabled"
-                        label="状态"
+                        label="操作"
                         show-overflow-tooltip>
                 </el-table-column>
             </el-table>
