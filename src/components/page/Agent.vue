991f07728ddfc3315437275189dc642dc50e3bd5
ethan-liu <ethan-liu@outlook.com>
2017-06-10 16:41:41 +0800

    修改经纪人页面

--- src/components/page/Agent.vue
+++ src/components/page/Agent.vue
@@ -8,21 +8,24 @@
         </div>
 
         <el-row>
-            <el-button type="primary">新增</el-button>
             <el-button type="primary">修改</el-button>
-            <el-button type="primary">删除</el-button>
             <el-button type="primary">停用</el-button>
             <el-button type="primary">隐藏停用</el-button>
+            <el-button type="primary">审批通过</el-button>
+            <el-button type="primary">审批不通过</el-button>
         </el-row>
         <el-row>
             <el-form :inline="true" :model="form">
-                <el-form-item label="中介编号：">
-                    <el-input v-model="form.id" placeholder="支持模糊查询"></el-input>
-                </el-form-item>
-                <el-form-item label="中介名称：">
+                <el-form-item label="姓名：">
                     <el-input v-model="form.name" placeholder="支持模糊查询"></el-input>
                 </el-form-item>
-                <el-form-item label="状态：">
+                <el-form-item label="所属门店：">
+                    <el-input v-model="form.name" placeholder="支持模糊查询"></el-input>
+                </el-form-item>
+                <el-form-item label="联系电话：">
+                    <el-input v-model="form.name" placeholder="支持模糊查询"></el-input>
+                </el-form-item>
+                <el-form-item label="人员状态：">
                     <el-select v-model="form.status" placeholder="请选择">
                         <el-option label="启用" value="1"></el-option>
                         <el-option label="停用" value="0"></el-option>
@@ -45,20 +48,32 @@
                 </el-table-column>
                 <el-table-column
                         prop="code"
-                        label="中介编号">
+                        label="人员编号">
                 </el-table-column>
                 <el-table-column
                         prop="name"
-                        label="中介名称">
+                        label="姓名">
                 </el-table-column>
                 <el-table-column
                         prop="branchAmount"
-                        label="门店数量">
+                        label="所属门店">
+                </el-table-column>
+                <el-table-column
+                        prop="branchAmount"
+                        label="联系电话">
+                </el-table-column>
+                <el-table-column
+                        prop="branchAmount"
+                        label="工号">
                 </el-table-column>
                 <el-table-column
                         prop="enabled"
-                        label="状态"
+                        label="人员状态"
                         show-overflow-tooltip>
+                </el-table-column>
+                <el-table-column
+                        prop="branchAmount"
+                        label="操作">
                 </el-table-column>
             </el-table>
             <div class="pagination">
